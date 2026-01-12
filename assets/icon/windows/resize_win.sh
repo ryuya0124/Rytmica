4696ea58513e50c122a0208e48ea31d5460feb7c
ryuya <ryuya1adgjmptw@gmail.com>
2026-01-12 15:33:07 +0900

    ロゴ増やした

---
+++ assets/icon/windows/resize_win.sh
@@ -0,0 +1,5 @@
+magick icon.jpg \
+    -resize 300x300 -write icon-300.jpg -write icon-300.png \
+    -resize 256x256 -write icon-256.jpg -write icon-256.png \
+    -resize 150x150 -write icon-150.jpg -write icon-150.png \
+    -resize 71x71   -write icon-71.jpg          icon-71.png
